8e13bb3f0a2cc7fa32de1d309938e6e4f405be46
lando2319 <mikepland@gmail.com>
2023-04-01 06:37:18 -0500

    update file and epochs on createFineTune

--- createFineTune.js
+++ createFineTune.js
@@ -13,9 +13,9 @@ const openai = new OpenAIApi(configuration);
 (async () => {
     try {
         const response = await openai.createFineTune({
-            training_file: "file-AdlXs0tjdUcmy9rGCEVmScap",
+            training_file: "file-ZLoZVDfmO9Uur7UUUWa0GAKi",
             model: "davinci",
-            n_epochs: 8
+            n_epochs: 12
         });
 
         console.log("SUCCESS");
@@ -27,3 +27,9 @@ const openai = new OpenAIApi(configuration);
         process.exit(1);
     }
 })();
+
+// Core file: file-bwQ2C8WUkpRz9qliejJFggM0
+// OG File: file-peGiB9Vf7zniWF1qj0L4Y1IC
+
+// OG Model: "davinci:ft-personal-2023-02-26-12-50-08",
+// ADDED CORE
